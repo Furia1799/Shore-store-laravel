e49924573cb5decc1736215ddb5d435b6f5e67a9
Bryan Reyes <furia1799@gmail.com>
2021-04-27 21:33:20 -0500

    Agregro css carrito y modifico carrito en diseño

--- resources/views/producto/carrito.blade.php
+++ resources/views/producto/carrito.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,6 +15,7 @@
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="../assets/css/index.css">
+    <link rel="stylesheet" href="../assets/css/carrito.css">
 </head>
 <body>
 <header>
@@ -68,41 +68,62 @@
             </li>
         </ul>
     </nav>
+    <!--ENCABEZADO-->
     <div class="container-fluid">
         <div  class="row">
             <div class="col-lg-12">
                 <h1 id="productos_titulo" class="display-4 text-center">Carrito de Compras</h1>
             </div>
         </div>
-        <div  class="row">
-            <div id="imagen_prodcuto" class="col-lg-4">
+    </div>
+    <div class="container">
+        <!--CONTENIDO-->
+        <div  class="row align-items-center justify-content-center">
+            <div id="imagen_prodcuto" class="col-lg-3">
                 <a href="producto ">
-                    <img class="imagen_producto" src="../assets/img/tennis_1.jpg" class="rounded" alt="tennis_3" width="300" height="300">
-
-                    <p class="precio_producto"> $ 300 pesos</p>
+                    <img class="imagen_producto" src="../assets/img/tennis_1.jpg" class="rounded" alt="tennis_3"
+                         width="200" height="200">
                 </a>
             </div>
-            <div id="peque_descripccion_producuto" class="log-lg-4">
-                <h2 class="titulo_producto">Tennis Adidas</h2>
+            <div id="peque_descripccion_producuto" class="log-lg-3 text-center">
+                <h5 class="titulo_producto">Tennis Adidas</h5>
                 <p>Copa mundial</p>
                 <p>Talla : 21</p>
             </div>
-            <div id="cantidad_producto" class="col-lg-4">
-                <form action="#">
-                    <input type="number">
+            <div id="cantidad_producto" class="col-lg-3 text-center">
+                <form class="form-inline" action="#">
+                    <div class="form-group">
+                        <label for="cantidad" class="mr-sm-3">Cantidad: </label>
+                        <input type="number" class="form-control" min="1" max="10">
+                    </div>
                 </form>
             </div>
-            <div id="precio_producto" class="col-lg-4">
-
+            <div id="precio_producto" class="col-lg-3">
+                <p class="precio_producto">TOTAL $300.00 pesos</p>
             </div>
         </div>
-        <div  class="row">
-            <div id="imagen_prodcuto" class="col-lg-4">
+        <div  class="row align-items-center justify-content-center">
+            <div id="imagen_prodcuto" class="col-lg-3">
                 <a href="producto ">
-                    <img class="imagen_producto" src="../assets/img/tennis_1.jpg" class="rounded" alt="tennis_3" width="300" height="300">
-                    <h2 class="titulo_producto"> zapatoos negros</h2>
-                    <p class="precio_producto"> $ 300 pesos</p>
+                    <img class="imagen_producto" src="../assets/img/tennis_1.jpg" class="rounded" alt="tennis_3"
+                         width="200" height="200">
                 </a>
+            </div>
+            <div id="peque_descripccion_producuto" class="log-lg-3 text-center">
+                <h5 class="titulo_producto">Tennis Adidas</h5>
+                <p>Copa mundial</p>
+                <p>Talla : 21</p>
+            </div>
+            <div id="cantidad_producto" class="col-lg-3 text-center">
+                <form class="form-inline" action="#">
+                    <div class="form-group">
+                        <label for="cantidad" class="mr-sm-3">Cantidad: </label>
+                        <input type="number" class="form-control" min="1" max="10">
+                    </div>
+                </form>
+            </div>
+            <div id="precio_producto" class="col-lg-3">
+                <p class="precio_producto">TOTAL $300.00 pesos</p>
             </div>
         </div>
     </div>
