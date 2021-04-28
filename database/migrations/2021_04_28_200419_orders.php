9db6854d9027018a33d5a1ba2c78cb9e82bffa24
Bryan Reyes <furia1799@gmail.com>
2021-04-28 18:10:24 -0500

    Creamos migracion de 3 tablas

---
+++ database/migrations/2021_04_28_200419_orders.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Orders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function(Blueprint $table){
+            $table->id();
+            $table->foreignId('id_user');
+            $table->foreignId('id_direction');
+            $table->double('cost');
+            $table->string('status');
+            $table->dateTime('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
